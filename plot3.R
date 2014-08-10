be490eff963e9e53cab158178061da02b3c86269
slipsky <s_lipsky@hotmail.com>
2014-08-10 00:48:13 -0400

    Updates to pngs

--- plot3.R
+++ plot3.R
@@ -9,6 +9,8 @@ dev.off()
 plot.new()
 
 ##create graph, add in additional data lines, create legend
+png(file="plot3.png",width=480,height=480)
+
 plot(data$Date,data$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
 lines(data$Date,data$Sub_metering_2,type="l",col="red")
 lines(data$Date,data$Sub_metering_3,type="l",col="blue")
@@ -16,5 +18,4 @@ legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
        col=c("black","red","blue"),lwd=1,cex=0.7)
 
 ##save as png
-dev.copy(png,'plot3.png',width=480,height=480)
 dev.off()
--- plot4.R
+++ plot4.R
@@ -9,17 +9,19 @@ dev.off()
 plot.new()
 
 ##create graphs.  add in par() to arrange the four graphs in the right matrix
+png(file="plot4a.png",width=480,height=480)
 par(mfcol=c(2,2))
 plot(data$Date,data$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
 
 plot(data$Date,data$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
 lines(data$Date,data$Sub_metering_2,type="l",col="red")
 lines(data$Date,data$Sub_metering_3,type="l",col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"),lwd=1,cex=0.7)
 
 plot(data$Date,data$Voltage,type="l",xlab="datetime",ylab="Voltage")
 
 plot(data$Date,data$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
 
-##save as png
-dev.copy(png,'plot4.png',width=480,height=480)
+##save as png)
 dev.off()
